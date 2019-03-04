41aeb5d39aa451b1e29868bf93a2d7cecfdb9684
paveltech <pavelcep.sust11@gmail.com>
2019-03-04 16:01:08 +0600

    Initial commit

---
+++ app/src/main/java/com/dream71/android/barchartexample/PiChartActivity.java
@@ -0,0 +1,49 @@
+package com.dream71.android.barchartexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class PiChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pi_chart);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        PieChart pieChart = findViewById(R.id.piechart);
+        ArrayList NoOfEmp = new ArrayList();
+
+        NoOfEmp.add(new Entry(945f, 0));
+        NoOfEmp.add(new Entry(1040f, 1));
+        NoOfEmp.add(new Entry(1133f, 2));
+
+        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
+        dataSet.setValueTextSize(20);
+
+        ArrayList year = new ArrayList();
+
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+
+        PieData data = new PieData(year, dataSet);
+        pieChart.setData(data);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.animateXY(5000, 5000);
+    }
+
+}
